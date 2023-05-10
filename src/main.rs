80413f022ae42c954e3e99fd58f4b97695592d2e
Nics <NicsTr@users.noreply.github.com>
2023-05-10 14:28:23 +0200

    Done.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,73 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use nanos_sdk::buttons::ButtonEvent;
+use nanos_sdk::ecc::{Secp256k1, SeedDerive};
+use nanos_sdk::io;
+use nanos_sdk::io::ApduHeader;
+use nanos_ui::ui;
+
+nanos_sdk::set_panic!(nanos_sdk::exiting_panic);
+
+pub const AGE_BIP32_PATH: [u32; 4] = nanos_sdk::ecc::make_bip32_path(b"m/414745'/0'/0'/0'");
+
+#[no_mangle]
+extern "C" fn sample_main() {
+    let mut comm = io::Comm::new();
+
+    loop {
+        ui::SingleMessage::new("App age").show();
+
+        // Wait for either a specific button push to exit the app
+        // or an APDU command
+        match comm.next_event() {
+            io::Event::Button(ButtonEvent::BothButtonsRelease) => nanos_sdk::exit_app(0),
+            io::Event::Command(ins) => match handle_apdu(&mut comm, ins) {
+                Ok(()) => comm.reply_ok(),
+                Err(sw) => comm.reply(sw),
+            },
+            _ => (),
+        }
+    }
+}
+
+#[repr(u8)]
+enum Ins {
+    GetRecipient,
+    Unwrap,
+}
+
+impl From<ApduHeader> for Ins {
+    fn from(header: ApduHeader) -> Ins {
+        match header.ins {
+            2 => Ins::GetRecipient,
+            3 => Ins::Unwrap,
+            _ => panic!(),
+        }
+    }
+}
+
+use nanos_sdk::io::Reply;
+
+fn handle_apdu(comm: &mut io::Comm, ins: Ins) -> Result<(), Reply> {
+    if comm.rx == 0 {
+        return Err(io::StatusWords::NothingReceived.into());
+    }
+
+    match ins {
+        Ins::GetRecipient => {
+            let pk = Secp256k1::derive_from_path(&AGE_BIP32_PATH)
+                .public_key()
+                .map_err(|x| Reply(0x6eu16 | (x as u16 & 0xff)))?;
+            comm.append(pk.as_ref());
+        }
+        Ins::Unwrap => {
+            let sk = Secp256k1::derive_from_path(&AGE_BIP32_PATH);
+            let share = comm.get_data()?;
+            let ans = sk.ecdh(share)
+                .map_err(|_| Reply(0x6f00u16))?;
+            comm.append(ans.as_ref());
+        }
+    }
+    Ok(())
 }
---
+++ test/test_cmds.py
@@ -0,0 +1,51 @@
+import pytest
+import os
+from binascii import hexlify, unhexlify
+
+from ecpy.curves import Curve, Point
+from Crypto.Protocol.KDF import HKDF
+from Crypto.Hash import SHA256
+from chacha20poly1305 import ChaCha20Poly1305
+
+from ledgerwallet.client import LedgerClient
+
+
+d = LedgerClient()
+
+def exchange_and_expect(expected_output_hex: str, ins: int, data_hex: str = ''):
+    print(f"\n-> e0{ins:02x}0000{data_hex}")
+    r = d.apdu_exchange(ins, bytes.fromhex(data_hex))
+    print(f"<- {r.hex()}9000")
+    assert r.hex() == expected_output_hex.lower()
+
+
+def test_get_recipient():
+    expected = "04fb52b66f3256a5c28dbfcee45a78ff984d597473801360da90adae60c986b851d59103c870c70d3b26bc5d85424e4ff0420ba215a04fa3b0b51598cbdcec600f"
+
+    exchange_and_expect(expected, 0x02)
+
+def test_unwrap():
+    cv = Curve.get_curve('secp256k1')
+
+    recipient = d.apdu_exchange(0x2)
+    P = Point(int.from_bytes(recipient[1:33], "big"), int.from_bytes(recipient[33:65], "big"), cv)
+
+    file_key = os.urandom(16)
+    secret = int.from_bytes(os.urandom(32), "big")
+
+    basepoint = Point(0x79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798, 0x483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8, cv)
+    ephemeral_share = bytes(cv.encode_point(secret * basepoint))
+
+    salt = ephemeral_share + recipient
+    info = b"ledger"
+    shared_secret = bytes(cv.encode_point(secret * P))
+
+    wrap_key = HKDF(shared_secret, 32, salt, SHA256, 1, info)
+    cipher = ChaCha20Poly1305(wrap_key)
+    body = cipher.encrypt(b'\0'*12, file_key)
+
+    exchange_and_expect(shared_secret[1:33].hex(), 0x03, ephemeral_share.hex())
+
+
+#test_get_recipient()
+#test_unwrap()
